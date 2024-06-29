b87659aa048992d103c9eabb5a0228d4e2b62f52
iankityadav <1002ankit@gmail.com>
2024-06-29 13:21:16 +0530

    added routes and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { BookingComponent } from './booking/booking.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, BookingComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'booking-bistro';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { BookTableComponent } from './book-table/book-table.component';
+import { BookingComponent } from './booking/booking.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'book', component: BookTableComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'dashboard', component: HomeComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+];
