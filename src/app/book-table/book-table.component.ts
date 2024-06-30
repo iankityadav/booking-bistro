ee0958ba778ecfc6d0f920e42a199c420fda6a25
iankityadav <1002ankit@gmail.com>
2024-06-30 14:59:12 +0530

    book table ui

--- src/app/book-table/book-table.component.ts
+++ src/app/book-table/book-table.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-table',
   standalone: true,
   imports: [],
   templateUrl: './book-table.component.html',
-  styleUrl: './book-table.component.css'
+  styleUrl: './book-table.component.css',
 })
 export class BookTableComponent {
+  state: any;
+  constructor(private router: Router) {
+    console.log(this.router.getCurrentNavigation());
 
+    this.state = JSON.stringify(this.router.getCurrentNavigation()?.extras);
+  }
 }
